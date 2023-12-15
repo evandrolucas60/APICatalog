e1153b3e159ecf57d3d1ff7642688d52289b2072
evandrolucas60 <evandrolucas60@yahoo.com>
2023-12-15 16:28:14 -0300

    Applying Migrations to generate the database and tables

--- APICatalog/Context/ApplicationDbContext.cs
+++ APICatalog/Context/ApplicationDbContext.cs
@@ -5,9 +5,19 @@ namespace APICatalog.Context
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<Category>? Categories { get; set; }
         public DbSet<Product>? Products { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)"); 
+        }
     }
+
+
 }
