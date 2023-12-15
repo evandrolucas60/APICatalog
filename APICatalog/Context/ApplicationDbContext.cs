dbeac85543be012c87088c55aae4af47f2d39c6c
evandrolucas60 <evandrolucas60@yahoo.com>
2023-12-15 13:57:28 -0300

    Creating the EF Core context file

---
+++ APICatalog/Context/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using APICatalog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalog.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Product>? Products { get; set; }
+    }
+}
