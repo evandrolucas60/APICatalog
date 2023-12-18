8fbe6ffae1d7693cf683c16dccc81595cb0fb68d
evandrolucas60 <evandrolucas60@yahoo.com>
2023-12-18 14:41:08 -0300

    ProdutosController - DELETE : Deletes a product

--- APICatalog/Controllers/ProductsController.cs
+++ APICatalog/Controllers/ProductsController.cs
@@ -66,5 +66,18 @@ namespace APICatalog.Controllers
 
             return Ok(product);
         }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.ProductId == id);
+
+            if (product is null) return NotFound("Produto não localizado...");
+            
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+
+            return Ok(product);
+        }
     }
 }
