22241fad0a8de0fcdc630b6e1dacc11df1da407e
evandrolucas60 <evandrolucas60@yahoo.com>
2023-12-18 11:29:23 -0300

    Creating the ProductsController controller

---
+++ APICatalog/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using APICatalog.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalog.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
