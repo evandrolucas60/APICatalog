65cb0fd7f2737ad88a1d8ed47316ae6745ae95bf
evandrolucas60 <evandrolucas60@yahoo.com>
2024-08-15 16:05:28 -0300

    Paginação para Categorias - Implementação

--- ApiCatalogo/Repositories/CategoriaRepository.cs
+++ ApiCatalogo/Repositories/CategoriaRepository.cs
@@ -1,10 +1,18 @@
 ﻿using ApiCatalogo.Context;
 using ApiCatalogo.Models;
+using ApiCatalogo.Pagination;
 
 namespace ApiCatalogo.Repositories
 {
     public class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
     {
         public CategoriaRepository(AppDbContext context) : base(context) { }
+
+        public PagedList<Categoria> GetCategorias(CategoriasParameters categoriasParams)
+        {
+            var categorias = GetAll().OrderBy(c => c.CategoriaId).AsQueryable();
+            var categoriasOrdenadas = PagedList<Categoria>.ToPagedList(categorias, categoriasParams.PageNumber, categoriasParams.PageSize);
+            return categoriasOrdenadas;
+        }
     }
 }
