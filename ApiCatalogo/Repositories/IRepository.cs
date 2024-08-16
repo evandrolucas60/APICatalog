3ab79c5ecf290f742ed6f3f9a187df2a9a7647db
evandrolucas60 <evandrolucas60@yahoo.com>
2024-08-16 16:30:48 -0300

    Migrando o código Síncrono para Assíncrono - I

--- ApiCatalogo/Repositories/IRepository.cs
+++ ApiCatalogo/Repositories/IRepository.cs
@@ -1,12 +1,15 @@
 ﻿using ApiCatalogo.Models;
 using System.Linq.Expressions;
+using System.Runtime.Intrinsics.X86;
 
 namespace ApiCatalogo.Repositories
 {
     public interface IRepository<T>
     {
-        IEnumerable<T> GetAll();
-        T? Get(Expression<Func<T, bool>> predicate);
+        Task<IEnumerable<T>> GetAllAsync();
+
+        //<Func<T, bool>> predicate: Este parâmetro aceita uma expressão lambda que representa uma função que recebe um objeto do tipo T e retorna um valor booleano(true ou false)
+        Task<T?> GetAsync(Expression<Func<T, bool>> predicate);
         T Create(T entity);
         T Update(T entity);
         T Delete(T entity);
