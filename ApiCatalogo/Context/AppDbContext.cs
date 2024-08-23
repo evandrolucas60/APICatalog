a7adfa4b931189614b2d6eef03685c6c3d73b244
evandrolucas60 <evandrolucas60@yahoo.com>
2024-08-23 14:48:52 -0300

    Implementando a autenticação JWT Bearer - III

--- ApiCatalogo/Context/AppDbContext.cs
+++ ApiCatalogo/Context/AppDbContext.cs
@@ -10,5 +10,11 @@ public class AppDbContext : IdentityDbContext<ApplicationUser>
 
     public DbSet<Categoria> Categorias { get; set; }
     public DbSet<Produto> Produtos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+
 }
 
---
+++ ApiCatalogo/DTOs/TokenModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ApiCatalogo.DTOs
+{
+    public class TokenModel
+    {
+        public string? AcessToken { get; set; }
+        public string? RefreshToken { get; set; }
+    }
+}
